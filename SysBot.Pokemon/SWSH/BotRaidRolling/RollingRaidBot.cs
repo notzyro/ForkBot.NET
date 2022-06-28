ec11a90bd21da13b9843b13c43fbf3a66262b4a4
Koi <koi.github3088@gmail.com>
2022-06-27 19:18:14 -0500

    Slightly reduce scroll delay for rollover correction.

--- SysBot.Pokemon/SWSH/BotRaidRolling/RollingRaidBot.cs
+++ SysBot.Pokemon/SWSH/BotRaidRolling/RollingRaidBot.cs
@@ -718,7 +718,7 @@ namespace SysBot.Pokemon
             await PressAndHold(DDOWN, 2_000, 0_250, token).ConfigureAwait(false); // Scroll to system settings
             await Click(A, 1_250, token).ConfigureAwait(false);
 
-            await PressAndHold(DDOWN, 0_750 + Hub.Config.Timings.ExtraTimeScrollDownAR, 0_250, token).ConfigureAwait(false); // Scroll to date/time settings
+            await PressAndHold(DDOWN, 0_700 + Hub.Config.Timings.ExtraTimeScrollDownAR, 0_250, token).ConfigureAwait(false); // Scroll to date/time settings
             await Click(DDOWN, 0_150, token).ConfigureAwait(false);
 
             await Click(A, 1_250, token).ConfigureAwait(false);
