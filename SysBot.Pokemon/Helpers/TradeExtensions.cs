ca4085c4659a39172cf4ffaf3a7538b7d430bb8b
Koi <koi.github3088@gmail.com>
2022-07-02 16:19:42 -0500

    Unbreak ALM dependency update.

--- SysBot.Pokemon/Helpers/TradeExtensions.cs
+++ SysBot.Pokemon/Helpers/TradeExtensions.cs
@@ -162,7 +162,7 @@ namespace SysBot.Pokemon
             }
 
             pk = TrashBytes(pk);
-            pk.SetDynamaxLevel(0);
+            pk.SetDynamaxLevel();
 
             var la = new LegalityAnalysis(pk);
             var enc = la.EncounterMatch;
